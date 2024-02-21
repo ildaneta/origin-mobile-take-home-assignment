3c6303d25a32db93cc3f10a3d36acfbc7f1482e9
ildaneta <iildaemanoely@gmail.com>
2024-02-21 08:24:39 +0100

    feat: add config to use google fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded && fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

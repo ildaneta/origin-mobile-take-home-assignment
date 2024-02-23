cbd006c0f7c773104a696fe8bc0efa964d68c9a7
ildaneta <iildaemanoely@gmail.com>
2024-02-23 23:37:33 +0100

    feat: create state to control loading, add yup validation messages, usage of setUserData and setIsUserLogged actions, add error component and create logic to onSubmit method

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View } from 'tamagui';
+import { Alert } from 'react-native';
 
 import Container from '../components/Container';
 import Input from '../components/Input';
@@ -7,6 +8,7 @@ import HeaderBack from '../components/HeaderBack';
 import Text from '../components/Text';
 import Upload from '../components/Upload';
 import SolidButton from '../components/SolidButton';
+import ErrorMessage from '../components/ErrorMessage';
 
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
@@ -14,35 +16,76 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Eye } from '@tamagui/lucide-icons';
 
-const schema = Yup.object().shape({
-  name: Yup.string().required(),
-  email: Yup.string().required().email(),
-  password: Yup.string().required(),
-});
+import { ISignUp } from '../types/auth';
 
-type FormValues = {
-  name: string;
-  email: string;
-  password: string;
-};
+import firebase from '@react-native-firebase/app';
+import { setUserDataFS } from '../firebase/firestore/users';
+import { signUp } from '../firebase/auth';
+
+import { useUserStore } from '../stores/user';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('name is required'),
+  email: Yup.string().required('email is required').email().trim(),
+  password: Yup.string().required('password is required').min(8).max(8).trim(),
+});
 
 const SignUp = (): JSX.Element => {
   const [isNameFocused, setIsNameFocused] = useState(false);
   const [isEmailFocused, setIsEmailFocused] = useState(false);
   const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { setUserData, setIsUserLogged } = useUserStore();
 
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver<FormValues>(schema),
+    resolver: yupResolver<ISignUp>(schema),
     defaultValues: {
       name: '',
       email: '',
       password: '',
     },
   });
+
+  const onSubmit = async ({ email, password, name }: ISignUp) => {
+    setIsLoading(true);
+
+    await signUp({ email, password })
+      .then(async () => {
+        const user = firebase.auth().currentUser;
+
+        if (user) {
+          setUserData({ name, email: user.email, uid: user.uid });
+
+          await setUserDataFS({ email: user.email, uid: user.uid, name })
+            .then(() => {
+              setIsUserLogged({ isLogged: true });
+              // TODO navigate to home screen
+            })
+            .catch((err) =>
+              console.log('Error setting user data within users: ', err)
+            );
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+
+        if (error.code === 'auth/email-already-in-use') {
+          Alert.alert('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert('That email address is invalid!');
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <Container hasPaddingHorizontal={false}>
@@ -68,7 +111,11 @@ const SignUp = (): JSX.Element => {
             isFocused={isNameFocused}
             onBlur={() => setIsNameFocused(false)}
             onFocus={() => setIsNameFocused(true)}
+            autoCapitalize="none"
+            autoCorrect={false}
           />
+
+          {!!errors.name && <ErrorMessage message={errors?.name?.message} />}
 
           <View marginTop={12} />
 
@@ -81,6 +128,8 @@ const SignUp = (): JSX.Element => {
             onBlur={() => setIsEmailFocused(false)}
             onFocus={() => setIsEmailFocused(true)}
           />
+
+          {!!errors.email && <ErrorMessage message={errors?.email?.message} />}
 
           <View marginTop={12} />
 
@@ -95,6 +144,10 @@ const SignUp = (): JSX.Element => {
             iconButton={<Eye color={'$primary300'} />}
           />
 
+          {!!errors.password && (
+            <ErrorMessage message={errors?.password?.message} />
+          )}
+
           <View marginTop={12} />
 
           <Upload
@@ -106,7 +159,13 @@ const SignUp = (): JSX.Element => {
 
           <View marginTop={36} />
 
-          <SolidButton label="Create account" onPress={() => {}} />
+          <SolidButton
+            label="Create account"
+            onPress={handleSubmit(onSubmit)}
+            isLoading={isLoading}
+          />
+
+          <View marginBottom={20} />
         </View>
       </>
     </Container>

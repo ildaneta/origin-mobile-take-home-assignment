a01bcd38a180bbf26562b10b3f4f2a9022724394
ildaneta <iildaemanoely@gmail.com>
2024-02-23 23:36:08 +0100

    feat: create setUserData action and add new properties default values

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,26 +3,45 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { IUser, UserState } from '../types/user';
+import { IUser, IUserData, UserState } from '../types/user';
 
 type Actions = {
   setIsUserLogged: ({ isLogged }: Pick<IUser, 'isLogged'>) => void;
+  setUserData: ({ email, name, uid }: IUserData) => void;
   resetUserData: () => void;
 };
 
 const initialState: UserState = {
   userData: {
     isLogged: false,
+    uid: '',
+    name: '',
+    email: '',
   },
 };
 
 export const useUserStore = create<UserState & Actions>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       ...initialState,
 
       setIsUserLogged: ({ isLogged }) => {
-        set({ userData: { isLogged } });
+        const userState = get().userData;
+
+        set({ userData: { ...userState, isLogged } });
+      },
+
+      setUserData: ({ email, name, uid }) => {
+        const { isLogged } = get().userData;
+
+        set({
+          userData: {
+            isLogged,
+            email,
+            name,
+            uid,
+          },
+        });
       },
 
       resetUserData: () => {

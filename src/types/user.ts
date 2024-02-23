16e52248f27be356e602ee76c9626ad314c81203
ildaneta <iildaemanoely@gmail.com>
2024-02-23 23:38:09 +0100

    feat: create IUserData interface and extends IUserData within IUser

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,13 @@
-export interface IUser {
-  isLogged: boolean;
-}
-
 export type UserState = {
   userData: IUser;
 };
+
+export interface IUserData {
+  uid: string;
+  email: string | null;
+  name: string;
+}
+
+export interface IUser extends IUserData {
+  isLogged: boolean;
+}

2d0dfe0716f8e2e51c9681986ef0d2b4724a5e95
ildaneta <iildaemanoely@gmail.com>
2024-02-24 16:18:14 +0100

    refactor: add currentScreenName inside UserState and improve name property type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,14 @@
+import { StackRoutes } from '../routes/stack.routes';
+
 export type UserState = {
   userData: IUser;
+  currentScreenName: keyof StackRoutes;
 };
 
 export interface IUserData {
   uid: string;
   email: string | null;
-  name: string;
+  name: string | null;
 }
 
 export interface IUser extends IUserData {

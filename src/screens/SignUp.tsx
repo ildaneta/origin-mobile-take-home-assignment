6efbdc042e5655c40f2f6f31250ed0a486644056
ildaneta <iildaemanoely@gmail.com>
2024-02-24 23:00:40 +0100

    feat: create logic to validate if a photo is selected,  upload user selfie within firebase storage, update user data store data with photo url and create a component to show the selected image

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'tamagui';
+import { Image, View } from 'tamagui';
 import { Alert, SafeAreaView } from 'react-native';
 
 import Container from '../components/Container';
@@ -10,7 +10,7 @@ import Upload from '../components/Upload';
 import SolidButton from '../components/SolidButton';
 import ErrorMessage from '../components/ErrorMessage';
 
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
@@ -23,14 +23,24 @@ import { setUserDataFS } from '../firebase/firestore/users';
 import { signUp } from '../firebase/auth';
 
 import { useUserStore } from '../stores/user';
+import storage from '@react-native-firebase/storage';
 
 import { StackRoutes } from '../routes/stack.routes';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+
+import * as ImagePicker from 'expo-image-picker';
+
+import { RectButton } from 'react-native-gesture-handler';
+
+import { IS_IOS } from '../utils/device';
+
+import { uploadCurrentUserPhotoURL } from '../helpers/uploadCurrentUserPhotoURL';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('name is required'),
   email: Yup.string().required('email is required').email().trim(),
   password: Yup.string().required('password is required').min(8).max(8).trim(),
+  photo: Yup.string().required('photo is required'),
 });
 
 const SignUp = (): JSX.Element => {
@@ -42,8 +52,10 @@ const SignUp = (): JSX.Element => {
     isSecureTextEntryPasswordEnabled,
     setIsSecureTextEntryPasswordEnabled,
   ] = useState(true);
+  const [image, setImage] = useState('');
 
-  const { setUserData, setIsUserLogged, setCurrentScreenName } = useUserStore();
+  const { setUserData, setIsUserLogged, setCurrentScreenName, setFileName } =
+    useUserStore();
   const { goBack } =
     useNavigation<NavigationProp<StackRoutes, 'loginOptions'>>();
 
@@ -51,14 +63,43 @@ const SignUp = (): JSX.Element => {
     control,
     handleSubmit,
     formState: { errors },
+    setValue,
+    setError,
   } = useForm({
     resolver: yupResolver<ISignUp>(schema),
     defaultValues: {
       name: '',
       email: '',
       password: '',
+      photo: '',
     },
   });
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const { uri } = result.assets[0];
+
+      setImage(uri);
+      setValue('photo', uri);
+      setError('photo', { message: '' });
+    }
+  };
+
+  const uploadImage = async (uid: string) => {
+    const filename = image.substring(image.lastIndexOf('/') + 1);
+    const uploadUri = IS_IOS ? image.replace('file://', '') : image;
+
+    setFileName({ filename });
+
+    await storage().ref(`user-photo/${uid}/${filename}`).putFile(uploadUri);
+  };
 
   const onSubmit = async ({ email, password, name }: ISignUp) => {
     setIsLoading(true);
@@ -77,20 +118,38 @@ const SignUp = (): JSX.Element => {
           );
 
         if (user) {
-          setUserData({ name, email: user.email, uid: user.uid });
+          setUserData({
+            name,
+            email: user.email,
+            uid: user.uid,
+            photoUrl: image,
+          });
 
           await setUserDataFS({ email: user.email, uid: user.uid, name })
             .then(() => {
-              setIsUserLogged({ isLogged: true });
-              setCurrentScreenName({ currentScreenName: 'home' });
+              uploadImage(user.uid)
+                .then(() => {
+                  setIsUserLogged({ isLogged: true });
+                  setCurrentScreenName({ currentScreenName: 'home' });
+
+                  uploadCurrentUserPhotoURL();
+
+                  setIsLoading(false);
+                })
+                .catch((error) =>
+                  console.error('Error uploading user selfie: ', error)
+                )
+                .finally(() => {
+                  setIsLoading(false);
+                });
             })
             .catch((err) =>
-              console.log('Error setting user data within users: ', err)
+              console.error('Error setting user data within users: ', err)
             );
         }
       });
     } catch (error: any) {
-      console.log(error);
+      console.error('Error creating user account: ', error);
 
       if (error.code === 'auth/email-already-in-use') {
         Alert.alert('That email address is already in use!');
@@ -99,8 +158,6 @@ const SignUp = (): JSX.Element => {
       if (error.code === 'auth/invalid-email') {
         Alert.alert('That email address is invalid!');
       }
-    } finally {
-      setIsLoading(false);
     }
   };
 
@@ -114,7 +171,7 @@ const SignUp = (): JSX.Element => {
 
   const ActionButton = () => (
     <>
-      <View marginTop={36} />
+      <View marginTop={!!image ? 0 : 36} />
 
       <SolidButton
         label="Create account"
@@ -122,8 +179,47 @@ const SignUp = (): JSX.Element => {
         isLoading={isLoading}
       />
 
-      <View marginBottom={30} />
+      <View marginBottom={100} />
     </>
+  );
+
+  const SelectedImage = () => (
+    <View justifyContent="center" alignItems="center" marginBottom={30}>
+      <Text
+        fontSize={'$3'}
+        marginBottom={10}
+        color={'$primary200'}
+        alignSelf="flex-start"
+      >
+        Selected selfie
+      </Text>
+
+      <Image
+        source={{ uri: image }}
+        width={90}
+        height={90}
+        borderRadius={45}
+        marginBottom={10}
+      />
+
+      <RectButton
+        onPress={pickImage}
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#666',
+          width: '60%',
+          alignSelf: 'center',
+          padding: 6,
+          borderRadius: 12,
+        }}
+      >
+        <Text fontSize={'$3'} lineHeight={22} marginRight={4} color="$white">
+          Choose another image
+        </Text>
+      </RectButton>
+    </View>
   );
 
   return (
@@ -201,12 +297,27 @@ const SignUp = (): JSX.Element => {
 
           <View marginTop={12} />
 
-          <Upload
-            hasError={false}
-            onPress={() => {}}
-            allowedTypes="JPG, PNG, JPEG"
-            label="Upload your selfie"
-          />
+          {!!image ? (
+            <SelectedImage />
+          ) : (
+            <Controller
+              name="photo"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <Upload
+                  hasError={!!errors?.photo}
+                  onPress={pickImage}
+                  allowedTypes="JPG, PNG, JPEG"
+                  label="Upload your selfie"
+                />
+              )}
+            />
+          )}
+
+          {!!errors.password && (
+            <ErrorMessage message={errors?.photo?.message} />
+          )}
 
           <ActionButton />
         </>

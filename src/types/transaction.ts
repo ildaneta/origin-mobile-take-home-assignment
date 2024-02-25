a7ede5e59a879dfc04cc5633b8de471d7a68e749
ildaneta <iildaemanoely@gmail.com>
2024-02-25 01:30:47 +0100

    feat: create TransactionType and TransactionCategory enum

---
+++ src/types/transaction.ts
@@ -0,0 +1,12 @@
+export enum TransactionType {
+  invoice = 'invoice',
+  deposit = 'deposit',
+  payment = 'payment',
+  withdraw = 'withdraw',
+}
+
+export enum TransactionCategory {
+  shopping = 'shopping',
+  entertainment = 'entertainment',
+  drinks_and_dinning = 'drinks and dinning',
+}

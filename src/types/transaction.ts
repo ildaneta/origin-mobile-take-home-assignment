5e73cb3d69d1efbda3f107928efd90f6ca9a4a81
ildaneta <iildaemanoely@gmail.com>
2024-02-25 17:42:13 +0100

    feat: craete new interfaces and change the name from withdraw to withdrawal

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -2,11 +2,51 @@ export enum TransactionType {
   invoice = 'invoice',
   deposit = 'deposit',
   payment = 'payment',
-  withdraw = 'withdraw',
+  withdrawal = 'withdrawal',
 }
 
 export enum TransactionCategory {
   shopping = 'shopping',
   entertainment = 'entertainment',
   drinks_and_dinning = 'drinks and dinning',
+}
+
+export interface IGetTransactionsList {
+  pageNumber: number;
+  pageSize: number;
+}
+
+export interface IGetTransactionsDetails {
+  id: number;
+}
+
+export interface IGetTransactionsListResponse {
+  Page: number;
+  PageSize: number;
+  TotalRecords: number;
+  TotalPages: number;
+  Transactions: Array<ITransactionsItem>;
+}
+
+export interface IPostUpdateTransactionCoordinates {
+  id: number;
+  lat: number;
+  long: number;
+}
+
+export interface IPostUploadTransactionReceipt {
+  id: number;
+  receiptImageUrl: string;
+}
+
+export interface ITransactionsItem {
+  Id: number;
+  Amount: number;
+  Date: Date;
+  Vendor: string;
+  Type: TransactionType;
+  Category: TransactionCategory;
+  Lat: number;
+  Lon: number;
+  ReceiptImage: string | null;
 }

9f98a57e97903e5063c46e6083c4c435082934e4
ildaneta <iildaemanoely@gmail.com>
2024-02-25 17:52:17 +0100

    refactor: create logic to get and render transactions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,97 @@
-import React, { useCallback } from 'react';
-import { Image, View } from 'tamagui';
+import React, { useCallback, useState } from 'react';
+import { ActivityIndicator, RefreshControl } from 'react-native';
+
+import { Button, Image, View } from 'tamagui';
+import { CircleEllipsis } from '@tamagui/lucide-icons';
 
 import Container from '../../components/Container';
 import Text from '../../components/Text';
-
-import { useUserStore } from '../../stores/user';
 import TransactionItem from './components/TransactionItem';
 
+import { useUserStore } from '../../stores/user';
+
+import { OriginAPI } from '../../services/origin';
+
+import { useFocusEffect } from '@react-navigation/native';
+
+import { IGetTransactionsListResponse } from '../../types/transaction';
+
+import { SCREEN_HEIGHT } from '../../utils/device';
+
+import { FlashList } from '@shopify/flash-list';
+
 const Home = (): JSX.Element => {
+  let pageSize = 200;
+
+  const [transactionsList, setTransactionsList] =
+    useState<IGetTransactionsListResponse>();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
   const {
     userData: { photoUrl, name },
   } = useUserStore();
+
+  const getTransactions = async () => {
+    setIsRefreshing(true);
+
+    try {
+      const { data: transactionsListResponse } =
+        await OriginAPI.getTransactionsList({
+          pageNumber: 1,
+          pageSize,
+        });
+
+      if (transactionsListResponse) {
+        setTransactionsList(transactionsListResponse);
+        setCurrentPage(currentPage + 1);
+      }
+    } catch (error) {
+      console.error('Error getting transactions list', error);
+    } finally {
+      setIsRefreshing(false);
+    }
+  };
+
+  const loadMoreTransactions = async () => {
+    setIsLoading(true);
+
+    try {
+      if (transactionsList) {
+        if (currentPage < transactionsList.TotalPages) {
+          const { data: transactionsListResponse } =
+            await OriginAPI.getTransactionsList({
+              pageNumber: currentPage,
+              pageSize,
+            });
+
+          if (transactionsListResponse) {
+            setTransactionsList((prevList) => ({
+              ...prevList,
+              ...transactionsListResponse,
+              Transactions: [
+                ...(prevList?.Transactions ?? []),
+                ...(transactionsListResponse.Transactions ?? []),
+              ],
+            }));
+
+            setCurrentPage(currentPage + 1);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error getting transactions list', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getTransactions();
+    }, [])
+  );
 
   const Header = () => (
     <View marginTop={16} flexDirection="row" alignItems="center">
@@ -33,33 +114,67 @@ const Home = (): JSX.Element => {
     </View>
   );
 
-  const TransactionsList = () => (
-    <View marginTop={50}>
-      <Text
-        fontSize={'$4'}
-        color={'$primary700'}
-        fontWeight={'600'}
-        marginBottom={28}
-      >
+  const TransactionsListHeader = () => (
+    <View
+      marginTop={30}
+      marginBottom={20}
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Text fontSize={'$4'} color={'$primary700'} fontWeight={'600'}>
         Transactions
       </Text>
+
+      <Button unstyled pressStyle={{ opacity: 0.7 }}>
+        <CircleEllipsis color="$primary600" size={22} />
+      </Button>
     </View>
   );
 
   return (
-    <Container>
+    <Container hasScroll={false}>
       <>
         <Header />
 
-        <TransactionsList />
+        <TransactionsListHeader />
 
-        <TransactionItem
-          title="Vandervort, Parker and Barrows"
-          category="drinks and dinning"
-          type="payment"
-          ammount={104.84}
-          date="2024-01-25T13:29:18.5457245+00:00"
-        />
+        <View flexGrow={1} height={SCREEN_HEIGHT - 200}>
+          <FlashList
+            estimatedItemSize={transactionsList?.TotalRecords ?? 200}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={({ Id }) => String(Id)}
+            data={transactionsList?.Transactions}
+            ItemSeparatorComponent={() => (
+              <View marginBottom={5} borderWidth={1} borderColor={'$gray100'} />
+            )}
+            refreshControl={
+              <RefreshControl
+                refreshing={isRefreshing}
+                onRefresh={getTransactions}
+              />
+            }
+            onEndReached={loadMoreTransactions}
+            onEndReachedThreshold={0.1}
+            renderItem={({ item: transaction }) => (
+              <TransactionItem
+                vendor={transaction.Vendor}
+                category={transaction.Category}
+                type={transaction.Type}
+                ammount={transaction.Amount}
+                date={transaction.Date}
+                onPress={() => {}}
+              />
+            )}
+            ListFooterComponent={
+              isLoading ? (
+                <ActivityIndicator size="large" color="#111" />
+              ) : (
+                <></>
+              )
+            }
+          />
+        </View>
       </>
     </Container>
   );
